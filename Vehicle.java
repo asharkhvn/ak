bc275bdd05ece0a96f6b9c6c7e7c2f3b4a2687bd
asharkhvn <55751427+asharkhvn@users.noreply.github.com>
2019-10-10 11:56:13 -0400

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,33 @@
+package javaleads.lesson2;
+
+ abstract class Vehicle {
+	 
+	 String runVehicle;  
+	 
+	 public String printVehicleDetails; 
+	 
+	 class car extends Vehicle {
+	 
+		 public void main(String[] args) {
+		 
+	private int = engineCount; 
+	
+	//@Override 
+	
+	super.printVehicleDetails;  
+	
+	class Boat extends Vehicle; { 
+	
+		private int = engineCount;
+	
+		super.printVehicleDetails; 
+		
+		interface product {
+		public int addstock; 
+}
+
+ }
+		 
+	 
+	 
+ }
